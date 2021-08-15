54246d605eb746446d3efbbb9cf272c14ed1148c
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-08-15 07:00:42 -0400

    initial commit

---
+++ TestSomething/Code/Bookings.Reservations.sql
@@ -0,0 +1,17 @@
+﻿CREATE SEQUENCE Bookings.ReservationSequence AS INT START WITH 0 INCREMENT BY 1;
+GO
+CREATE TABLE Bookings.Reservations
+(
+  ReservationId INT 
+    CONSTRAINT [Booking.Reservations.ReservationId:DF] DEFAULT NEXT VALUE FOR Bookings.ReservationSequence
+    CONSTRAINT [Booking.Reservations:PK] PRIMARY KEY CLUSTERED,
+  CustomerId INT,
+  HotelId INT,
+  BookingDate DATETIME2,
+  StartDate DATE,
+  StayLengthInDays INT,
+  ReservationDate DATE,
+  AdditionalColumns BINARY(200),
+  CONSTRAINT [Bookings.Reservations:FK-->Bookings.Customers] 
+    FOREIGN KEY (CustomerId) REFERENCES Bookings.Customers(CustomerId)
+);
