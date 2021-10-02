75f29100848bb12f0e8e77d75790c8a11164f052
Liz Baron <10554+lizbaron@users.noreply.github.com>
2021-10-02 19:22:04 -0400

    restart reference project

--- VSOnlySolution/Tests/SomeRandomTests.sql
+++
@@ -1,1 +0,0 @@
-﻿CREATE SCHEMA [SomeRandomTests] AUTHORIZATION [tSQLt.TestClass];
--- VSOnlySolution/Tests/Test1.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE PROCEDURE SomeRandomTests.[Test1]
-AS
-	EXEC tSQLt.FakeTable @TableName = 'dbo.ARandomTable';
-	EXEC tSQLt.AssertEmptyTable @TableName = 'dbo.ARandomTable';
-
-RETURN 0
