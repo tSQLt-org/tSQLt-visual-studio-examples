2c5fd9cac86c63acc1de1aea2233dfdd224e2653
mbt1 <bitbucket@meine.us>
2021-10-17 20:44:54 -0400

    Cleanup

--- tSQLtVSTemplate/Code/dbo.CalculateDiscount.sql
+++ tSQLtVSTemplate/Code/dbo.CalculateDiscount.sql
@@ -1,14 +1,13 @@
-﻿/*
+﻿/*--
 DROP FUNCTION IF EXISTS dbo.CalculateDiscount;
 GO
 --*/
-CREATE FUNCTION dbo.CalculateDiscount
+CREATE FUNCTION [dbo].[CalculateDiscount]
 (
-	@v NUMERIC(13,2)
+	@Value NUMERIC(13,2)
 )
 RETURNS TABLE
 AS
 RETURN
-	SELECT CASE WHEN @v>=50.00 THEN @v*0.10 ELSE 0 END AS discount;
+  SELECT CASE WHEN @Value >= 50 THEN @Value * 0.10 ELSE 0 END discount;
 GO
-
--- tSQLtVSTemplate/Tests/dbo_CalculateDiscountTests.sql
+++ tSQLtVSTemplate/Tests/dbo_CalculateDiscountTests.sql
@@ -1,42 +1,45 @@
 ﻿/*--
-EXEC tSQLt.DropClass dbo_CalculateDiscountTests;
+EXEC tSQLt.DropClass [dbo_CalculateDiscountTests];
 GO
 --*/
-CREATE SCHEMA dbo_CalculateDiscountTests AUTHORIZATION [tSQLt.TestClass];
+CREATE SCHEMA [dbo_CalculateDiscountTests] AUTHORIZATION [tSQLt.TestClass];
 GO
-CREATE PROCEDURE dbo_CalculateDiscountTests.[test no discount for order value 49]
+CREATE PROCEDURE dbo_CalculateDiscountTests.[test returns no discount for order value 49.00]
 AS
 BEGIN
   --Assemble
-  
+
   --Act
-  DECLARE @Actual NUMERIC(13,2) = (SELECT discount FROM dbo.CalculateDiscount(49.00));
+  DECLARE @Actual NUMERIC(13,2) = (SELECT discount FROM dbo.CalculateDiscount(49.00) AS CD);
 
   --Assert
   EXEC tSQLt.AssertEquals @Expected = 0, @Actual = @Actual;
+
 END;
 GO
-CREATE PROCEDURE dbo_CalculateDiscountTests.[test returns 10% discount for oder value 51]
+CREATE PROCEDURE dbo_CalculateDiscountTests.[test returns 10% discount for order value 51.00]
 AS
 BEGIN
   --Assemble
-  
+
   --Act
-  DECLARE @Actual NUMERIC(13,2) = (SELECT discount FROM dbo.CalculateDiscount(51.00));
+  DECLARE @Actual NUMERIC(13,2) = (SELECT discount FROM dbo.CalculateDiscount(51.00) AS CD);
 
   --Assert
-  EXEC tSQLt.AssertEquals @Expected = 5.1, @Actual = @Actual;
+  EXEC tSQLt.AssertEquals @Expected = 5.10, @Actual = @Actual;
+
 END;
 GO
-CREATE PROCEDURE dbo_CalculateDiscountTests.[test returns 10% discount for oder value 50]
+CREATE PROCEDURE dbo_CalculateDiscountTests.[test returns 10% discount for order value 50.00]
 AS
 BEGIN
   --Assemble
-  
+
   --Act
-  DECLARE @Actual NUMERIC(13,2) = (SELECT discount FROM dbo.CalculateDiscount(50.00));
+  DECLARE @Actual NUMERIC(13,2) = (SELECT discount FROM dbo.CalculateDiscount(50.00) AS CD);
 
   --Assert
-  EXEC tSQLt.AssertEquals @Expected = 5.0, @Actual = @Actual;
+  EXEC tSQLt.AssertEquals @Expected = 5.00, @Actual = @Actual;
+
 END;
 GO
