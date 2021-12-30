62928afe899ecf6a9c068cd1845940f74ff7107f
mbt1 <bitbucket@meine.us>
2021-12-30 16:55:16 -0500

    getting SSIS Packages fully parameterized

---
+++ tSQLtSSISTemplate/TemplateSSIS.CI/SetVariableValues.sql
@@ -0,0 +1,20 @@
+﻿EXEC [SSISDB].[catalog].[create_environment] @environment_name=N'TemplateSSIS', @environment_description=N'', @folder_name=N'TestFolder.ref'
+GO
+DECLARE @var sql_variant = N'Data Source=.,41419;User ID=tSQLt.Build.SSIS;Initial Catalog=tSQLt_Dev;Provider=SQLNCLI11.1;Application Name=SourceDBConnection;Auto Translate=False; '
+EXEC [SSISDB].[catalog].[create_environment_variable] @variable_name=N'SourceDB_ConnectionString', @sensitive=False, @description=N'', @environment_name=N'TemplateSSIS', @folder_name=N'TestFolder.ref', @value=@var, @data_type=N'String'
+GO
+DECLARE @var sql_variant = N'Data Source=.,41419;User ID=tSQLt.Build.SSIS;Initial Catalog=tSQLt_Dev;Provider=SQLNCLI11.1;Application Name=TargetDBConnection;Auto Translate=False; '
+EXEC [SSISDB].[catalog].[create_environment_variable] @variable_name=N'TargetDB_ConnectionString', @sensitive=False, @description=N'', @environment_name=N'TemplateSSIS', @folder_name=N'TestFolder.ref', @value=@var, @data_type=N'String'
+GO
+DECLARE @var SQL_VARIANT = N''
+EXEC [SSISDB].[catalog].[create_environment_variable] @variable_name=N'SourceDB_Password', @sensitive=True, @description=N'', @environment_name=N'TemplateSSIS', @folder_name=N'TestFolder.ref', @value=@var, @data_type=N'String'
+GO
+DECLARE @var SQL_VARIANT = N''
+EXEC [SSISDB].[catalog].[create_environment_variable] @variable_name=N'TargetDB_Password', @sensitive=True, @description=N'', @environment_name=N'TemplateSSIS', @folder_name=N'TestFolder.ref', @value=@var, @data_type=N'String'
+GO
+
+GO
+
+
+GO
+
---
+++ tSQLtSSISTemplate/TemplateSSIS.CI/VariableReferenceMapping.sql
@@ -0,0 +1,16 @@
+﻿Declare @reference_id bigint
+EXEC [SSISDB].[catalog].[create_environment_reference] @environment_name=N'TemplateSSIS', @environment_folder_name=N'TestFolder.ref', @reference_id=@reference_id OUTPUT, @project_name=N'TemplateSSIS', @folder_name=N'TestFolder', @reference_type=A
+Select @reference_id
+GO
+EXEC [SSISDB].[catalog].[set_object_parameter_value] @object_type=20, @parameter_name=N'SourceDB_ConnectionString', @object_name=N'TemplateSSIS', @folder_name=N'TestFolder', @project_name=N'TemplateSSIS', @value_type=R, @parameter_value=N'SourceDB_ConnectionString'
+GO
+EXEC [SSISDB].[catalog].[set_object_parameter_value] @object_type=20, @parameter_name=N'SourceDB_Password', @object_name=N'TemplateSSIS', @folder_name=N'TestFolder', @project_name=N'TemplateSSIS', @value_type=R, @parameter_value=N'SourceDB_Password'
+GO
+EXEC [SSISDB].[catalog].[set_object_parameter_value] @object_type=20, @parameter_name=N'TargetDB_ConnectionString', @object_name=N'TemplateSSIS', @folder_name=N'TestFolder', @project_name=N'TemplateSSIS', @value_type=R, @parameter_value=N'TargetDB_ConnectionString'
+GO
+EXEC [SSISDB].[catalog].[set_object_parameter_value] @object_type=20, @parameter_name=N'TargetDB_Password', @object_name=N'TemplateSSIS', @folder_name=N'TestFolder', @project_name=N'TemplateSSIS', @value_type=R, @parameter_value=N'TargetDB_Password'
+GO
+
+GO
+
+
