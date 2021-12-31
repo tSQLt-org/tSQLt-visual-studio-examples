3d2a504ff8a823c2e73f936e6f7108a45fefcf5f
mbt1 <bitbucket@meine.us>
2021-12-31 07:00:00 -0500

    delete and create folders

---
+++ tSQLtSSISTemplate/TemplateSSIS.CI/CreateSSISFolder.sql
@@ -0,0 +1,4 @@
+﻿DECLARE @folder_id BIGINT
+EXEC [SSISDB].[catalog].[create_folder] @folder_name=N'TestFolder', @folder_id=@folder_id OUTPUT
+SELECT @folder_id
+EXEC [SSISDB].[catalog].[set_folder_description] @folder_name=N'TestFolder', @folder_description=N'Ephemeral folder for testing!'
---
+++ tSQLtSSISTemplate/TemplateSSIS.CI/DeleteSSISFolder.sql
@@ -0,0 +1,28 @@
+﻿(MAX) = 'TestFolder';
+DECLARE @cmd NVARCHAR(MAX) = 
+ISNULL(
+  (
+    SELECT 'EXEC [SSISDB].[catalog].[delete_project] @project_name=N'''+P.name+''',@folder_name=N'''+F.name+''';'
+      FROM SSISDB.catalog.folders AS F
+      JOIN SSISDB.catalog.projects AS P
+        ON P.folder_id = F.folder_id
+     WHERE F.name = @FolderName
+       FOR XML PATH(''),TYPE
+  ).value('.','NVARCHAR(MAX)')
+,'') + 
+ISNULL(
+  (
+    SELECT 'EXEC [SSISDB].[catalog].[delete_environment] @environment_name=N'''+E.name+''',@folder_name=N'''+F.name+''';'
+      FROM SSISDB.catalog.folders AS F
+      JOIN SSISDB.catalog.environments AS E
+        ON E.folder_id = F.folder_id
+     WHERE F.name = @FolderName
+       FOR XML PATH(''),TYPE
+  ).value('.','NVARCHAR(MAX)')
+,'') + 
+(SELECT 'EXEC [SSISDB].[catalog].[delete_folder] @folder_name=N'''+F.name+''';'
+  FROM SSISDB.catalog.folders AS F
+ WHERE F.name = @FolderName);
+
+--PRINT @cmd
+EXEC(@cmd);
