d770ce55e206d1fa30c14abe0df560b63bd20a80
mbt1 <bitbucket@meine.us>
2022-01-02 08:33:08 -0500

    It worked - kind of. and only once.

---
+++ tSQLtSSISTemplate/TemplateDatabase/TransformedObjectList.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TransformedObjectList]
+(
+    [name] nvarchar(128),
+    [SchemaName] nvarchar(128),
+    [QuotedSchemaName] nvarchar(258),
+    [QuotedName] nvarchar(258),
+    [QuotedFullName] nvarchar(517)
+)
--- tSQLtSSISTemplate/TemplateSSISTests/TransformObjectListTests.sql
+++ tSQLtSSISTemplate/TemplateSSISTests/TransformObjectListTests.sql
@@ -1,15 +1,18 @@
 ﻿CREATE SCHEMA TransformObjectListTests AUTHORIZATION [tSQLt.TestClass]
 GO
+--[@tSQLt:NoTransaction](DEFAULT)
 CREATE PROCEDURE TransformObjectListTests.[test SSIS is cool]
 AS
 BEGIN
-  DECLARE @package_name nvarchar(200) = N'TransformObjectList.dtsx';
-  DECLARE @project_name nvarchar(200) = N'TemplateSSIS';
-  DECLARE @folder_name nvarchar(200) = N'TestFolder';
+  DECLARE @package_name nvarchar(260) = N'TransformObjectList.dtsx';
+  DECLARE @project_name nvarchar(128) = N'TemplateSSIS';
+  DECLARE @folder_name nvarchar(128) = N'TestFolder';
+  DECLARE @environment_name nvarchar(128) = N'TemplateSSIS';
+  DECLARE @environment_folder_name nvarchar(128) = N'TestFolder.ref';
 
   -- SSIS Execution Parameters
   DECLARE @execution_id bigint;
-  DECLARE @reference_id smallint = null;
+  DECLARE @reference_id bigint = null;
 
   -- SSIS Catalog Variables (object_type = 50)
   DECLARE @logging_level smallint = 3;
@@ -25,12 +28,31 @@ BEGIN
   /* Run the SSIS Package						              */
   /**********************************************************/
 
-  EXEC [SSISDB].[catalog].[create_execution] @package_name=@package_name, @execution_id=@execution_id OUTPUT, @folder_name=@folder_name, @project_name=@project_name, @use32bitruntime=False, @reference_id=@reference_id;
+SELECT @reference_id = ER.reference_id
+  FROM SSISDB.catalog.environment_references AS ER
+  JOIN SSISDB.catalog.projects AS P
+    ON P.project_id = ER.project_id
+  JOIN SSISDB.catalog.folders AS F
+    ON F.folder_id = P.folder_id
+ WHERE F.name = @folder_name
+   AND P.Name = @project_name
+   AND ER.environment_folder_name = @environment_folder_name
+   AND ER.environment_name = @environment_name
+
+
+  EXEC [SSISDB].[catalog].[create_execution] 
+      @package_name=@package_name, @execution_id=@execution_id OUTPUT, 
+      @folder_name=@folder_name, @project_name=@project_name, 
+      @use32bitruntime=False, @reference_id= @reference_id, @runinscaleout=False
+  SELECT @execution_id
 
   EXEC [SSISDB].[catalog].[set_execution_parameter_value] @execution_id, @object_type = 50, @parameter_name=N'LOGGING_LEVEL', @parameter_value=@logging_level;
   EXEC [SSISDB].[catalog].[set_execution_parameter_value] @execution_id, @object_type = 50, @parameter_name=N'SYNCHRONIZED', @parameter_value=@is_synchronized;
 
-  EXEC [SSISDB].[catalog].[start_execution] @execution_id;
+
+  EXEC [SSISDB].[catalog].[start_execution] @execution_id
+
+
 
 END;
 GO
